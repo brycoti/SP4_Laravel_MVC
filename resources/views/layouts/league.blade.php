02ca815c53dd66be5093f0cb1a739e0220eb1fe5
brycoti <bryancozartitos@gmail.com>
2023-09-14 07:32:31 +0200

    mejorando maquetacion

--- resources/views/layouts/league.blade.php
+++ resources/views/layouts/league.blade.php
@@ -8,39 +8,42 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
+</head>
+
 <body class="bg-gray-100">
 
     <header class="bg-blue-600 text-white p-6">
         <div class="container mx-auto">
-            <div class="flex justify-between items-center">
+            <div class="flex justify-between items-center space-x-4">
                 <h1 class="text-3xl font-semibold">Football League</h1>
-                <a href="{{route('teams.index')}}" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M7.293 9.293a1 1 0 011.414 0L12 12.586V3a1 1 0 112 0v9.586l3.293-3.293a1 1 0 011.414 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                    </svg>
-                    Come back
-                </a>
-                <div class="text-center">
-        <a href="{{route('teams.create')}}" 
-            class="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition duration-300">
-            Create Team
-        </a>
-    </div>
+                <nav class="space-x-4">
+                    <a href="{{route('home')}}" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300">
+                        <svg class="w-5 h-5 mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">...</svg>
+                        Home
+                    </a>
+                    <a href="{{route('teams.index')}}" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300">
+                        <svg class="w-5 h-5 mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">...</svg>
+                        Teams
+                    </a>
+                    <a href="{{route('teams.create')}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300">
+                        Create Team
+                    </a>
+                </nav>
             </div>
         </div>
     </header>
 
-
     <!-- Contenido Principal -->
-    <main class="container mx-auto p-4">
+    <main class="container mx-auto p-4 my-6">
         @yield('content')
     </main>
 
     <!-- Footer o Pie de página -->
-    <footer class="bg-blue-600 text-white p-4 text-center mt-4">
-        <p>Liga de Fútbol &copy; 2023</p>
+    <footer class="bg-blue-600 text-white p-4 text-center">
+        <p>your football &copy; 2023</p>
     </footer>
 
 </body>
 
 </html>
+
--- resources/views/teams/createTeams.blade.php
+++ resources/views/teams/createTeams.blade.php
@@ -1,23 +1,44 @@
 @extends('layouts.league')
 
 @section('content')
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+        <!-- Heading -->
+        <div class="bg-blue-600 text-white p-6 mb-6 rounded focus:outline-none">
+            <h1 class="text-2xl font-semibold text-center">Here you can create your New Team!</h1>
+        </div>
+
+        <!-- Team Creation Form -->
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg flex items-center justify-center">
+            <form action="{{ route('teams.store') }}" method="POST" class="max-w-xl">
+                @csrf
+
+                <!-- Team Name Input -->
+                <div class="mb-6 inline-block items-center">
+                    <label for="name" class="block text-3xl text-black-600 mb-4 text-center">
+                        Name:
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        class=" text-center shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                        placeholder="Enter team name">
+                </div>
+
+                <!-- Submit Button -->
+                <div class="px-4 mt-12 block items-center">
+                    <button
+                        type="submit"
+                        class="inline-flex px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out">
+                        Create New Team
+                    </button>
+                </div>
+            </form>
+        </div>
 
 
-<div class="container h-full">
-    <span><h1>Here create a new team!</h1></span>
-
-    <form action="{{route('teams.store')}}" method="POST">
-
-        @csrf
-
-        <label>
-            Name:
-            <br>
-            <input type="name" name="name">
-        </label>
-        <br>
-        <button type="submit">Create New Team</button>
-    </form>
+    </div>
 </div>
-
 @endsection
